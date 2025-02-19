5e00f79f40b32a55ac2e06ddbdc232aee3adc31e
Rashim-Sunar <rashimsunar.02@gmail.com>
2025-02-18 22:53:07 -0500

    inline function and refied

--- Day_7_Generics/Refied.kt
+++ Day_7_Generics/Refied.kt
@@ -3,6 +3,7 @@ Reified Type Parameters (reified) in Inline Functions
 Normally, generic types are erased at runtime (type erasure). But reified allows us to retain the generic type at runtime.
 */
 
+/* 
 inline fun <reified T> isType(value: Any): Boolean {
     return value is T
 }
@@ -11,6 +12,32 @@ fun main() {
     println(isType<String>("Hello")) // true
     println(isType<Int>("Hello"))    // false
 }
+*/
 
 // Without reified, T would be erased at runtime.
 // With reified, we can check the type at runtime.
+
+
+
+// A program to get specific data from the mixed list
+fun main() {
+    val mixedList = mutableListOf(1,2,3,'a', 'b','c',"Hello", "World")
+    
+    val specificList : List<Char> = getSpecificList<Char>(mixedList)
+    
+    for(element : Char in specificList){
+        print("$element, ")    // a, b, c, 
+    }
+}
+
+inline fun <reified T> getSpecificList(list : List<Any>) : List<T>{
+    val specificList : MutableList<T> = mutableListOf<T>()
+    
+    for(element : Any in list){
+        if(element is T){
+            specificList.add(element)
+        }
+    }
+    
+    return specificList
+}
